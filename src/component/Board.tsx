b8a403f20385a94a0c2d34a2266066bfb5fae375
MalloryChin <Mallory.Chin@du.edu>
2020-01-28 16:11:13 +0800

    seperate Square out from Board

--- src/component/Board.tsx
+++ src/component/Board.tsx
@@ -1,24 +1,5 @@
 import * as React from 'react';
-
-interface SquareProps {
-  value: number;
-  selected: boolean;
-  onClick: () => void;
-}
-
-class Square extends React.Component<SquareProps> {
-  render() {
-    let className = "square";
-    if(this.props.selected) {
-      className += " selected_square";
-    }
-    return (
-      <button className={className} onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
-}
+import Square from './Square';
 
 interface BoardProps {
   size: number;

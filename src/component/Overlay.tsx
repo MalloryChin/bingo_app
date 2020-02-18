d496d9f5cfc7dd564206aca88eca9f02e7b2df30
Mallory Chin <menghsuanchin@gmail.com>
2020-02-18 18:23:39 +0800

    Hook useContext

--- src/component/Overlay.tsx
+++ src/component/Overlay.tsx
@@ -6,25 +6,25 @@ interface OverlayProps {
 	onClick: () => void;
 }
 
-class Overlay extends React.Component<OverlayProps> {
-	static contextType = OverlayTheme;
-	render() {
-		let overlayClass = 'game-overlay';
-		/*console.log(this.context);*/
-		if(this.context) {
-			overlayClass = 'game-overlay overlay-on';
-		}
-		return (
-			<div className={overlayClass}>
-				<div className="center-div">
-					<div id="victory-text">勝利！</div>
-					<button id="restart-btn" onClick={() => this.props.onClick()}>
-						重新開始
-					</button>
-				</div>
-			</div>
-		);
+function Overlay(props: OverlayProps) {
+	const context = React.useContext(OverlayTheme);
+
+	let overlayClass = 'game-overlay';
+	if(context) {
+		overlayClass = 'game-overlay overlay-on';
 	}
+
+
+	return (
+		<div className={overlayClass}>
+			<div className="center-div">
+				<div id="victory-text">勝利！</div>
+				<button id="restart-btn" onClick={() => props.onClick()}>
+					重新開始
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Overlay;

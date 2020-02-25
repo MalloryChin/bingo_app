f0135a03d22ce5e190d00bc25c049fa5ad9c6e7d
Mallory Chin <menghsuanchin@gmail.com>
2020-02-25 12:27:36 +0800

    checkpoint

--- src/week2/BPtable.tsx
+++ src/week2/BPtable.tsx
@@ -4,18 +4,22 @@ import {formContext} from './AppWeek2';
 
 function BPtable() {
 	const context = useContext(formContext);
+	const columnNames = ['姓名','手機','email','生日'];
 
 	const cellRenderer = (rowIndex: number, colIndex: number) => {
 		return <Cell>{context[rowIndex][colIndex]}</Cell>;
 	};
 
+	const columns = columnNames.map((columnName) => {
+		return (
+			<Column key={columnName} name={columnName} cellRenderer={cellRenderer} />
+		);
+	});
+
 	const numRows = context.length;
 	return (
 		<Table numRows={numRows}>
-			<Column name="姓名" cellRenderer={cellRenderer} />
-			<Column name="手機" cellRenderer={cellRenderer} />
-			<Column name="email" cellRenderer={cellRenderer} />
-			<Column name="生日" cellRenderer={cellRenderer} />
+			{columns}
 		</Table>
 	);
 }
